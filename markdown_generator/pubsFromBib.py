4cd48681ee2e201c2c7c5b1531c7a9982fc9fafd
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-09-30 15:06:26 +0200

    Homepage initial update

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -27,12 +27,12 @@ import re
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
     "proceeding": {
-        "file" : "proceedings.bib",
+        "file" : "pubs.bib",
         "venuekey": "booktitle",
         "venue-pretext": "In the proceedings of ",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
-        
+
     },
     "journal":{
         "file": "pubs.bib",
@@ -40,7 +40,7 @@ publist = {
         "venue-pretext" : "",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
-    } 
+    }
 }
 
 html_escape_table = {
@@ -151,7 +151,7 @@ for pubsource in publist:
 
             md_filename = os.path.basename(md_filename)
 
-            with open("../_publications/" + md_filename, 'w') as f:
+            with open("../_publications/" + md_filename, 'w', encoding="utf-8") as f:
                 f.write(md)
             print(f'SUCESSFULLY PARSED {bib_id}: \"', b["title"][:60],"..."*(len(b['title'])>60),"\"")
         # field may not exist for a reference
